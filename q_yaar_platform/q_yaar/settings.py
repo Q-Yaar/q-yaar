3a3e68337cb27b04ec30200d76d4e666ab52c5ae
Shishir Prashanth <shishir@esamudaay.com>
2025-12-07 18:16:50 +0530

    allowed cors headers

--- q_yaar_platform/q_yaar/settings.py
+++ q_yaar_platform/q_yaar/settings.py
@@ -74,6 +74,7 @@ PROJECT_APPS = [
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + PROJECT_APPS
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "log_request_id.middleware.RequestIDMiddleware",  # Logging-Request-ID
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
@@ -234,6 +235,20 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_LIFETIME": timedelta(minutes=5),
     "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=1),
 }
+
+# CORS
+CORS_ALLOWED_ORIGINS = ["*"]
+CORS_ALLOW_ALL_HEADERS = True
+# CORS_ALLOW_HEADERS = [
+#     "authorization",
+#     "content-type",
+#     "accept",
+#     "origin",
+#     "user-agent",
+#     "x-csrftoken",
+#     "x-requested-with",
+# ]
+
 
 #######################################################################################################################
 
