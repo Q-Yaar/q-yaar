1edfcd7519e84a0eaaa92a353ae85cea8d56f8b7
Shishir Prashanth <shishir@esamudaay.com>
2026-02-07 14:54:11 +0530

    admin page for facts

--- q_yaar_platform/fact/admin.py
+++ q_yaar_platform/fact/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Fact
+
+
+class FactAdmin(admin.ModelAdmin):
+    list_display = ("game", "target_team", "fact_type", "created", "modified")
+    search_fields = ["external_id", "game__game_code", "target_team__team_name"]
+    list_filter = ("fact_type",)
+
+    # Override to use the base manager to include soft-deleted items
+    def get_queryset(self, request):
+        return self.model._base_manager.get_queryset()
+
+
+admin.site.register(Fact, FactAdmin)
