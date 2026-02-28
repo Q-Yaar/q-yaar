9ab45ab609e9456b584a9b03d849bf8c54e49067
Parag Jyoti Nath <paragjnath@gmail.com>
2026-03-01 02:12:39 +0530

    added locations to admin

--- q_yaar_platform/location/admin.py
+++ q_yaar_platform/location/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Location, LocationSharingSetting
 
-# Register your models here.
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'player', 'game', 'team', 'lat', 'lon', 'timestamp', 'client')
+    list_filter = ('client', 'timestamp')
+    readonly_fields = ('player', 'game', 'team', 'lat', 'lon', 'accuracy', 'timestamp', 'client')
+
+@admin.register(LocationSharingSetting)
+class LocationSharingSettingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'player', 'is_sharing_enabled', 'tracking_code')
+    list_filter = ('is_sharing_enabled',)
