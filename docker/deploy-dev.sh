5ea0bcb42fb8936e9e37489cefc909e16e4ea9b1
Shishir Prashanth <shishir@esamudaay.com>
2026-05-28 00:57:51 +0530

    updated local docker setup

--- docker/deploy-dev.sh
+++ docker/deploy-dev.sh
@@ -1,26 +1,27 @@
-run_db="false"
-run_redisearch="false"
+collectstatic="false"
+
+PROJECT_HOME="$(cd "$(dirname "$0")/.." && pwd)"
 
 for i in $*; do
   if [ "$i" = "--help" ]; then
-    echo "Usage: sh deploy-staging.sh [options]"
+    echo "Usage: sh deploy-dev.sh [options]"
     echo "--help | prints usage info"
-    echo "--run-db | Runs postgres db in container"
-    echo "--run-redisearch | Starts redisearch in container"
+    echo "--collectstatic | if collectstatic command needs to be run"
     exit 0
-  elif [ "$i" = "--run-db" ]; then
-    run_db="true"
-  elif [ "$i" = "--run-redisearch" ]; then
-    run_redisearch="false"
+  elif [ "$i" = "--collectstatic" ]; then
+    collectstatic="true"
   fi
 done
 
-docker compose -f docker-compose-dev.yml up --build -d q_yaar_core fast_worker slow_worker celery_beat
+echo "Setting variables"
+. $PROJECT_HOME/q_yaar_platform/.env_docker
 
-if [ "$run_db" = "true" ]; then
-  docker compose -f docker-compose-dev.yml up -d db
-fi
+docker compose -f $PROJECT_HOME/docker/docker-compose-dev.yml build
+docker compose -f $PROJECT_HOME/docker/docker-compose-dev.yml up -d
 
-if [ "$run_redisearch" = "true" ]; then
-  docker compose -f docker-compose-dev.yml up -d redisearch
+docker compose -f $PROJECT_HOME/docker/docker-compose-dev.yml exec q_yaar_core python manage.py migrate
+
+if [ "$collectstatic" = "true" ]; then
+  docker compose -f $PROJECT_HOME/docker/docker-compose-dev.yml exec q_yaar_core mkdir -p static
+  docker compose -f $PROJECT_HOME/docker/docker-compose-dev.yml exec q_yaar_core python manage.py collectstatic --noinput
 fi
