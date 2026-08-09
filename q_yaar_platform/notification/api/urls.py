1cb60be13b03fa1c456a072a2fde167e985dbd26
Shishir Prashanth <shishir@esamudaay.com>
2026-08-09 17:47:30 +0530

    api endpoint change for notifications read

--- q_yaar_platform/notification/api/urls.py
+++ q_yaar_platform/notification/api/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path(r"webpush/keys", views.WebPushKeysView.as_view(), name="webpush-keys"),
     path(r"webpush/subscribe", views.WebPushSubscribeView.as_view(), name="webpush-subscribe"),
-    path(r"<uuid:notification_id>", views.NotificationReadView.as_view(), name="notification-read"),
+    path(r"<uuid:notification_id>/read", views.NotificationReadView.as_view(), name="notification-read"),
     path(r"", views.NotificationListView.as_view(), name="notification-history"),
 ]
