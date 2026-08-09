f5e3dbc3d7651bdf855dbbfed2e33f6d82bd2831
Shishir Prashanth <shishir@esamudaay.com>
2026-08-09 14:16:22 +0530

    initializing celery

--- q_yaar_platform/notification/api/serializers.py
+++ q_yaar_platform/notification/api/serializers.py
@@ -7,7 +7,7 @@ class NotificationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = PushNotificationHistory
-        fields = ["notification_id", "title", "message", "payload", "is_read", "created", "updated"]
+        fields = ["notification_id", "title", "message", "payload", "is_read", "created", "modified"]
 
     def get_notification_id(self, obj: PushNotificationHistory):
         return str(obj.get_external_id())
