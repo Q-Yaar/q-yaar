caef4e21cc0ddafb1e65dea4bd707d7851712d3c
Sireesh Kodali <sireesh@siruknown.net>
2026-08-30 21:30:41 +0530

    media: Add API to upload/download assets from S3
    generic API to get pre-signed URLs

--- q_yaar_platform/media/admin.py
+++ q_yaar_platform/media/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Asset
+
+
+class AssetAdmin(admin.ModelAdmin):
+    list_display = ("external_id", "status", "asset_name")
+    search_fields = ["external_id", "object_key", "asset_name"]
+    list_filter = ("status",)
+    readonly_fields = ("external_id", "object_key", "uploaded_by", "created", "modified")
+
+
+admin.site.register(Asset, AssetAdmin)
