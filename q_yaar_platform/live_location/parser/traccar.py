995d9923fee430d4edd704996809d6926302dd69
Shishir Prashanth <shishir@esamudaay.com>
2026-08-30 17:42:04 +0530

    testing traccar live location upgrade

--- q_yaar_platform/live_location/parser/traccar.py
+++ q_yaar_platform/live_location/parser/traccar.py
@@ -3,9 +3,33 @@ from live_location.parser.response_format import LocationResponseFormat
 
 
 class TraccarParser(AbstractLocationParser):
-    def parse(self, data: dict) -> LocationResponseFormat:
-        lat = data["location"]["coords"]["latitude"]
-        lon = data["location"]["coords"]["longitude"]
-        accuracy = data["location"]["coords"]["accuracy"]
-
-        return LocationResponseFormat(lat=lat, lon=lon, raw_data=data, accuracy=accuracy)
+    def parse(self, data) -> LocationResponseFormat:
+        if "lat" in data and "lon" in data:
+            lat = float(data.get("lat"))
+            lon = float(data.get("lon"))
+            
+            accuracy_val = data.get("accuracy")
+            accuracy = 0.0
+            if accuracy_val:
+                try:
+                    accuracy = float(accuracy_val)
+                except ValueError:
+                    pass
+            return LocationResponseFormat(lat=lat, lon=lon, raw_data=data, accuracy=accuracy)
+            
+        elif "location" in data and "coords" in data["location"]:
+            coords = data["location"]["coords"]
+            lat = float(coords["latitude"])
+            lon = float(coords["longitude"])
+            
+            accuracy_val = coords.get("accuracy")
+            accuracy = 0.0
+            if accuracy_val:
+                try:
+                    accuracy = float(accuracy_val)
+                except ValueError:
+                    pass
+            return LocationResponseFormat(lat=lat, lon=lon, raw_data=data, accuracy=accuracy)
+            
+        else:
+            raise KeyError("Unable to parse location data: missing latitude/longitude")
